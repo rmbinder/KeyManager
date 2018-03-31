e16a93bc97e00631ccfccd93aa23fd0e77246fe8
rmbinder <r-m-b@gmx.net>
2018-03-31 10:20:52 +0200

    addLanguageFolderPath is no longer required

--- keymanager.php
+++ keymanager.php
@@ -34,9 +34,6 @@ require_once(__DIR__ . '/common_function.php');
 require_once(__DIR__ . '/classes/keys.php');
 require_once(__DIR__ . '/classes/configtable.php');
 
-// Einbinden der Sprachdatei
-$gL10n->addLanguageFolderPath(ADMIDIO_PATH . FOLDER_PLUGINS . PLUGIN_FOLDER. '/languages');
-
 // only authorized user are allowed to start this module
 if (!isUserAuthorized($_SERVER['SCRIPT_NAME']))
 {
