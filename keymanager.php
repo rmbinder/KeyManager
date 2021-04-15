65d57f8305071e24e92d379b96fdbece54a48cd9
rmbinder <r-m-b@gmx.net>
2021-04-15 17:28:42 +0200

    update version to 2.0.1

--- keymanager.php
+++ keymanager.php
@@ -3,7 +3,7 @@
  ***********************************************************************************************
  * KeyManager
  *
- * Version 2.0.0
+ * Version 2.0.1
  *
  * KeyManager is an Admidio plugin for managing building and room keys.
  * 
