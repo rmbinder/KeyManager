0a373c1d5d7e119089fbc29c422950c6024ae16f
rmbinder <r-m-b@gmx.net>
2021-12-09 16:14:20 +0100

    formfiller security check built in, #19

--- keys_export_to_pff.php
+++ keys_export_to_pff.php
@@ -24,22 +24,32 @@ require_once(__DIR__ . '/classes/configtable.php');
 // Initialize and check the parameters
 $getKeyId = admFuncVariableIsValid($_GET, 'key_id',  'int');
 
+$pkmArray = array();
+
 $pPreferences = new ConfigTablePKM();
 $pPreferences->read();
+$pPreferences->readPff();
 
 if (substr_count($gNavigation->getUrl(), 'keys_export_to_pff') === 1)
 {
 	admRedirect(ADMIDIO_URL . FOLDER_PLUGINS . PLUGIN_FOLDER. '/keymanager.php');
 	// => EXIT
 }
-    
+
+if (!array_key_exists($pPreferences->config['Optionen']['interface_pff'], $pPreferences->configpff['Formular']['desc']))
+{
+    $gMessage->show($gL10n->get('PLG_KEYMANAGER_PFF_CONFIG_NOT_FOUND'));
+}
+else 
+{
+    $pkmArray['form_id'] = $pPreferences->config['Optionen']['interface_pff'];
+}
+
 $keys = new Keys($gDb, $gCurrentOrgId);
 $keys->readKeyData($getKeyId, $gCurrentOrgId);
 
 // add current url to navigation stack
 $gNavigation->addUrl(CURRENT_URL);
-
-$pkmArray = array();
     	
 foreach($keys->mKeyFields as $keyField)
 {    		
@@ -71,8 +81,6 @@ foreach($keys->mKeyFields as $keyField)
     
     $pkmArray['kmf-'. $kmfNameIntern] = $content;
 }
-
-$pkmArray['form_id'] = $pPreferences->config['Optionen']['interface_pff'];
 
 admRedirect(SecurityUtils::encodeUrl(ADMIDIO_URL . FOLDER_PLUGINS .'/'.$pPreferences->pffDir().'/createpdf.php', $pkmArray));
     		
