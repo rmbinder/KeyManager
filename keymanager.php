6fee105afcc80d204a7f9515644abaa63f6a93e6
rmbinder <r-m-b@gmx.net>
2021-12-27 23:55:55 +0100

    libs folder renamed

--- keymanager.php
+++ keymanager.php
@@ -144,7 +144,7 @@ switch ($getMode)
         $classTable  = 'table table-condensed table-striped';
         break;
     case 'xlsx':
-	    include_once(__DIR__ . '/vendor/PHP_XLSXWriter/xlsxwriter.class.php');
+	    include_once(__DIR__ . '/libs/PHP_XLSXWriter/xlsxwriter.class.php');
 	    $getMode     = 'xlsx';
 	    break;
     default:
