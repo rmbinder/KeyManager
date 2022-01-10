69c00bd44a99be0a5a7e82952253cb3d8e53cdfa
rmbinder <r-m-b@gmx.net>
2022-01-10 19:35:45 +0100

    new year 2022

--- synchronize.php
+++ synchronize.php
@@ -3,7 +3,7 @@
  ***********************************************************************************************
  * Makes a user to a former if he does not have a key.
  *
- * @copyright 2004-2021 The Admidio Team
+ * @copyright 2004-2022 The Admidio Team
  * @see https://www.admidio.org/
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  * 
