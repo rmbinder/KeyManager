fcfcb95546e54d4072ba2fb2253fb4d9b68cdcf6
rmbinder <r-m-b@gmx.net>
2023-01-23 14:28:19 +0100

    new year 2023

--- keymanager_profile_addin.php
+++ keymanager_profile_addin.php
@@ -11,7 +11,7 @@
  * require_once(ADMIDIO_PATH . FOLDER_PLUGINS .'/keymanager/keymanager_profile_addin.php');
  *
  *
- * @copyright 2004-2022 The Admidio Team
+ * @copyright 2004-2023 The Admidio Team
  * @see https://www.admidio.org/
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  *
