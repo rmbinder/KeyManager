4b0a821f577ad06ba61f45e5a0051569d38a43cb
rmbinder <r-m-b@gmx.net>
2025-12-12 20:20:25 +0100

    function_exists built-in, #39

--- system/common_function.php
+++ system/common_function.php
@@ -35,97 +35,102 @@ if (! defined('TBL_KEYMANAGER_LOG')) {
     define('TBL_KEYMANAGER_LOG', $g_tbl_praefix . '_keymanager_log');
 }
 
-spl_autoload_register('myAutoloader');
+if (! function_exists('myAutoloader')) {
 
-/**
- * Mein Autoloader
- * Script aus dem Netz
- * https://www.marcosimbuerger.ch/tech-blog/php-autoloader.html
- * @param   string  $className   Die übergebene Klasse
- * @return  string  Der überprüfte Klassenname
- */
-function myAutoloader($className) {
-    // Projekt spezifischer Namespace-Prefix.
-    $prefix = 'Plugins\\';
-    
-    // Base-Directory für den Namespace-Prefix.
-    $baseDir = __DIR__ . '/../../';
-    
-    // Check, ob die Klasse den Namespace-Prefix verwendet.
-    $len = strlen($prefix);
-    
-    if (strncmp($prefix, $className, $len) !== 0) {
-        // Wenn der Namespace-Prefix nicht verwendet wird, wird abgebrochen.
-        return;
-    }
-    // Den relativen Klassennamen ermitteln.
-    $relativeClassName = substr($className, $len);
-    
-    // Den Namespace-Präfix mit dem Base-Directory ergänzen,
-    // Namespace-Trennzeichen durch Verzeichnis-Trennzeichen im relativen Klassennamen ersetzen,
-    // .php anhängen.
-    $file = $baseDir . str_replace('\\', '/', $relativeClassName) . '.php';
-    // Pfad zur Klassen-Datei zurückgeben.
-    if (file_exists($file)) {
-        require $file;
+    /**
+     * Mein Autoloader
+     * Script aus dem Netz
+     * https://www.marcosimbuerger.ch/tech-blog/php-autoloader.html
+     *
+     * @param string $className
+     *            Die übergebene Klasse
+     * @return string Der überprüfte Klassenname
+     */
+    function myAutoloader($className)
+    {
+        // Projekt spezifischer Namespace-Prefix.
+        $prefix = 'Plugins\\';
+
+        // Base-Directory für den Namespace-Prefix.
+        $baseDir = __DIR__ . '/../../';
+
+        // Check, ob die Klasse den Namespace-Prefix verwendet.
+        $len = strlen($prefix);
+
+        if (strncmp($prefix, $className, $len) !== 0) {
+            // Wenn der Namespace-Prefix nicht verwendet wird, wird abgebrochen.
+            return;
+        }
+        // Den relativen Klassennamen ermitteln.
+        $relativeClassName = substr($className, $len);
+
+        // Den Namespace-Präfix mit dem Base-Directory ergänzen,
+        // Namespace-Trennzeichen durch Verzeichnis-Trennzeichen im relativen Klassennamen ersetzen,
+        // .php anhängen.
+        $file = $baseDir . str_replace('\\', '/', $relativeClassName) . '.php';
+        // Pfad zur Klassen-Datei zurückgeben.
+        if (file_exists($file)) {
+            require $file;
+        }
     }
 }
 
+spl_autoload_register('myAutoloader');
+
 /**
  * Funktion prueft, ob der Nutzer berechtigt ist das Plugin auszuführen.
- * 
+ *
  * In Admidio im Modul Menü kann über 'Sichtbar für' die Sichtbarkeit eines Menüpunkts eingeschränkt werden.
  * Der Zugriff auf die darunter liegende Seite ist von dieser Berechtigung jedoch nicht betroffen.
- * 
+ *
  * Mit Admidio 5 werden alle Startcripte meiner Plugins umbenannt zu index.php
  * Um die index.php auszuführen, kann die bei einem Menüpunkt angegebene URL wie folgt angegeben sein:
  * /adm_plugins/<Installationsordner des Plugins>
- *   oder
+ * oder
  * /adm_plugins/<Installationsordner des Plugins>/
- *   oder
+ * oder
  * /adm_plugins/<Installationsordner des Plugins>/<Dateiname.php>
- * 
+ *
  * Das Installationsscript des Plugins erstellt automatisch einen Menüpunkt in der Form: /adm_plugins/<Installationsordner des Plugins>/index.php
  * Standardmäßig wird deshalb für die Prüfung index.php als <Dateiname.php> verwendet, alternativ die übergebene Datei ($scriptname).
- * 
+ *
  * Diese Funktion ermittelt nur die Menüpunkte, die einen Dateinamen am Ende (index.php oder $scriptname) aufweisen, liest bei diesen Menüpunkten
  * die unter 'Sichtbar für' eingetragenen Rollen ein und prüft, ob der angemeldete Benutzer Mitglied mindestens einer dieser Rollen ist.
  * Wenn ja, ist der Benutzer berechtigt, das Plugin auszuführen (auch, wenn es weitere Menüpunkte ohne Dateinamen am Ende gibt).
  * Wichtiger Hinweis: Sind unter 'Sichtbar für' keine Rollen angegeben, so darf jeder Benutzer das Plugin ausführen
- * 
- * @param   string  $scriptName   Der Scriptname des Plugins (default: 'index.php')
- * @return  bool    true, wenn der User berechtigt ist
+ *
+ * @param string $scriptName
+ *            Der Scriptname des Plugins (default: 'index.php')
+ * @return bool true, wenn der User berechtigt ist
  */
-function isUserAuthorized( string $scriptname = '')
+function isUserAuthorized(string $scriptname = '')
 {
     global $gDb, $gCurrentUser;
-    
+
     $userIsAuthorized = false;
     $menIds = array();
-    
-    $menuItemURL = FOLDER_PLUGINS. PLUGIN_FOLDER. '/'. ((strlen($scriptname) === 0) ? 'index.php' : $scriptname);
-    
+
+    $menuItemURL = FOLDER_PLUGINS . PLUGIN_FOLDER . '/' . ((strlen($scriptname) === 0) ? 'index.php' : $scriptname);
+
     $sql = 'SELECT men_id
-              FROM '.TBL_MENU.'
+              FROM ' . TBL_MENU . '
              WHERE men_url = ? -- $menuItemURL';
-    
-    $menuStatement = $gDb->queryPrepared($sql, array($menuItemURL));
-    
-    if ( $menuStatement->rowCount() !== 0 )
-    {
-        while ($row = $menuStatement->fetch())
-        {
+
+    $menuStatement = $gDb->queryPrepared($sql, array(
+        $menuItemURL
+    ));
+
+    if ($menuStatement->rowCount() !== 0) {
+        while ($row = $menuStatement->fetch()) {
             $menIds[] = (int) $row['men_id'];
         }
-        
-        foreach ($menIds as $menId)
-        {
+
+        foreach ($menIds as $menId) {
             // read current roles rights of the menu
             $displayMenu = new RolesRights($gDb, 'menu_view', $menId);
-            
+
             // check for right to show the menu
-            if (count($displayMenu->getRolesIds()) === 0 || $displayMenu->hasRight($gCurrentUser->getRoleMemberships()))
-            {
+            if (count($displayMenu->getRolesIds()) === 0 || $displayMenu->hasRight($gCurrentUser->getRoleMemberships())) {
                 $userIsAuthorized = true;
             }
         }
@@ -147,7 +152,7 @@ function isUserAuthorizedForPreferences()
     // Konfiguration einlesen
     $pPreferences = new ConfigTable();
     $pPreferences->read();
-    
+
     $userIsAuthorized = false;
 
     if ($gCurrentUser->isAdministrator()) // Mitglieder der Rolle Administrator dürfen "Preferences" immer aufrufen
